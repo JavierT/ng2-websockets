10894a71006df3510e0c54ea6e16bb2a6c4477ad
Javier Tresaco <JavierT@users.noreply.github.com>
2018-03-13 17:57:56 +0100

    ngOnDestroy for closing WS

--- client/app/chat/chat.component.ts
+++ client/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {ChatService, Message} from '../shared/services/chat.service';
 
 @Component({
@@ -24,4 +24,9 @@ export class ChatComponent implements OnInit {
 			this.messages.push(msg);
 		});
 	}
+	
+	ngOnDestroy() {
+    		this.chatService.messages.unsubscribe();
+    		this.chatService.close()
+  	}
 }
